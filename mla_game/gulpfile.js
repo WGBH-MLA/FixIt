bde7d17c0937c230dc13a18362babad95f7350aa
alex_jones <alex_jones@wgbh.org>
2016-04-19 14:59:20 -0400

    Initial Commit

---
+++ mla_game/gulpfile.js
@@ -0,0 +1,44 @@
+var browserify = require('browserify'),
+    gulp = require('gulp'),
+    babelify = require('babelify'),
+    sass = require('gulp-ruby-sass'),
+    source = require('vinyl-source-stream'),
+    paths = {
+        mainJS: './global-static/javascript/main.js',
+        watchJS: './global-static/javascript/*',
+        mainScss: './global-static/scss/main.scss',
+        watchScss: './global-static/sass/**'
+    };
+
+gulp.task('js', [], function () {
+    var b = browserify();
+    b.transform(babelify);
+    b.add(paths.mainJS);
+    return b.bundle()
+        .pipe(source('main.js'))
+        .pipe(gulp.dest('./global-static/dist'));
+});
+
+gulp.task('scss', function () {
+    return sass(paths.mainScss, {
+        require: 'breakpoint',
+        compass: true
+    })
+    .on('error', function (err) {
+        console.error('Error: ', err.message);
+    })
+    .pipe(gulp.dest('./global-static/dist'));
+});
+
+gulp.task('build', ['js', 'scss'], function () {
+
+});
+
+gulp.task('watch', [], function () {
+    gulp.watch(paths.watchJS, ['js']);
+    gulp.watch(paths.watchScss, ['scss']);
+});
+
+gulp.task('default', ['build', 'watch'], function () {
+
+});
---
+++ mla_game/settings_template.py
@@ -0,0 +1,12 @@
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+    }
+}
