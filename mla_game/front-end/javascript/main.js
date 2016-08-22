91802b4091a0caa6d76d224d9e3d18150e2913a7
Jay Thompson <jay_thompson@wgbh.org>
2016-08-22 11:21:20 -0400

    Some basic functionality

--- mla_game/front-end/javascript/main.js
+++ mla_game/front-end/javascript/main.js
@@ -8,25 +8,24 @@ var swapClass;
 
 document.getElementById("submit").onclick = nextPhraseButtons;
 
-function swapClass () {
+function swapClassEvent (event) {
     console.log('doing swap class');
-    document.getElementById("phrase1").classList.remove('list-group-item-success');
-    document.getElementById("phrase1").classList.add('list-group-item-danger');
+    if (event.srcElement.classList.contains('list-group-item-success')) {
+        event.srcElement.classList.remove('list-group-item-success');
+        event.srcElement.classList.add('list-group-item-danger');
+    }
+    else if (event.srcElement.classList.contains('list-group-item-danger')) {
+        event.srcElement.classList.remove('list-group-item-danger');
+        event.srcElement.classList.add('list-group-item-success');
+    }
 }
 
-document.getElementById("phrase1").onclick = swapClass;
+var phraseAnchors = document.getElementsByClassName('list-group-item');
 
-var transcriptRequest = new XMLHttpRequest();
-var transcriptData;
-transcriptRequest.open('GET', '/api/transcript/random/');
-transcriptRequest.send();
-transcriptRequest.onreadystatechange = function () {
-    if (transcriptRequest.readyState == 4 && (transcriptRequest.status == 200)) {
-        transcriptData = JSON.parse(transcriptRequest.responseText);
-        mediaURL (transcriptData.transcript);
-        nextPhraseButtons (transcriptData.phrases);
-    }
-};
+var i;
+for (i = 0; i < phraseAnchors.length; i++) {
+    phraseAnchors[i].onclick = swapClassEvent;
+}
 
 function mediaURL (transcript_id) {
     var mediaRequest = new XMLHttpRequest();
@@ -45,6 +44,18 @@ function mediaURL (transcript_id) {
     };
 }
 
+var transcriptRequest = new XMLHttpRequest();
+var transcriptData;
+transcriptRequest.open('GET', '/api/transcript/random/');
+transcriptRequest.send();
+transcriptRequest.onreadystatechange = function () {
+    if (transcriptRequest.readyState == 4 && (transcriptRequest.status == 200)) {
+        transcriptData = JSON.parse(transcriptRequest.responseText);
+        mediaURL (transcriptData.transcript);
+        nextPhraseButtons (transcriptData.phrases);
+    }
+};
+
 function nextPhraseButtons (transcriptPhrases) {
     var target;
     for (var x=0; x < 3; x++) {
@@ -61,5 +72,9 @@ function nextPhraseButtons (transcriptPhrases) {
             console.log(target);
         }
         target.textContent = transcriptData.phrases.shift().text;
+        if (target.classList.contains('list-group-item-danger')) {
+            target.classList.remove('list-group-item-danger');
+            target.classList.add('list-group-item-success');
+        }
     }
 }
