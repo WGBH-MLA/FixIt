ef08db4a12028f677ac663d6c1255be73dea67cd
Paul Kinchla <paul_kinchla@wgbh.org>
2016-09-19 16:47:04 -0400

    markup for testing react and setup. added sample component with rules for naming

--- mla_game/front-end/dist/main.js
+++ mla_game/front-end/dist/main.js
@@ -1,4 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+// imports
 'use strict';
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
@@ -14,93 +15,161 @@ var _reactDom2 = _interopRequireDefault(_reactDom);
 window.React = _react2['default'];
 window.ReactDOM = _reactDom2['default'];
 
-// var mountNode = document.getElementById('target');
+var appTarget = document.getElementById('app');
+// component names should always begin with an uppercase letter
+// state = owned by current component
+// props = handed down from parent component
+/* use _ as a prefix for custom functions
+   // custom function
+   _clickHandler: function(){} 
+   // native to react    
+   getInitialState: function(){}
+*/
 
-// var HelloMessage = React.createClass({
-//   render: function() {
-//     return <div>React {this.props.name}</div>;
-//   }
-// });
+// test component
+var ComponentTest = _react2['default'].createClass({
+  displayName: 'ComponentTest',
 
-// ReactDOM.render(<HelloMessage name="Loaded Using NPM" />, mountNode);
-
-function reqListener() {
-    console.log(this.responseText);
-}
-
-var swapClass;
-
-document.getElementById("submit").onclick = nextPhraseButtons;
-
-function swapClassEvent(event) {
-    console.log('doing swap class');
-    if (event.srcElement.classList.contains('list-group-item-success')) {
-        event.srcElement.classList.remove('list-group-item-success');
-        event.srcElement.classList.add('list-group-item-danger');
-    } else if (event.srcElement.classList.contains('list-group-item-danger')) {
-        event.srcElement.classList.remove('list-group-item-danger');
-        event.srcElement.classList.add('list-group-item-success');
-    }
-}
-
-var phraseAnchors = document.getElementsByClassName('list-group-item');
-
-var i;
-for (i = 0; i < phraseAnchors.length; i++) {
-    phraseAnchors[i].onclick = swapClassEvent;
-}
-
-function mediaURL(transcript_id) {
-    var mediaRequest = new XMLHttpRequest();
-    var mediaData;
-    mediaRequest.open('GET', '/api/media/' + transcript_id + '/');
-    mediaRequest.send();
-    mediaRequest.onreadystatechange = function () {
-        var player = document.getElementById('player');
-        var playersrc = document.getElementById('mp3src');
-        if (mediaRequest.readyState == 4 && mediaRequest.status == 200) {
-            mediaData = JSON.parse(mediaRequest.responseText);
-            console.log(mediaData.media);
-            playersrc.src = mediaData.media;
-            player.load();
-        }
+  getInitialState: function getInitialState() {
+    return {
+      key: 'value',
+      count: 0
     };
-}
+  },
 
-var transcriptRequest = new XMLHttpRequest();
-var transcriptData;
-transcriptRequest.open('GET', '/api/transcript/random/');
-transcriptRequest.send();
-transcriptRequest.onreadystatechange = function () {
-    if (transcriptRequest.readyState == 4 && transcriptRequest.status == 200) {
-        transcriptData = JSON.parse(transcriptRequest.responseText);
-        mediaURL(transcriptData.transcript);
-        nextPhraseButtons(transcriptData.phrases);
-        document.getElementById('title').textContent = 'Title: ' + transcriptData.series;
-        document.getElementById('station').textContent = 'Station: ' + transcriptData.station;
-    }
+  _clickHandler: function _clickHandler() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  },
+
+  render: function render() {
+    return _react2['default'].createElement(
+      'div',
+      null,
+      _react2['default'].createElement(
+        'h2',
+        null,
+        'Simple React ',
+        this.props.name
+      ),
+      _react2['default'].createElement(
+        'button',
+        { className: 'app-counter', onClick: this._clickHandler },
+        'Change Count'
+      ),
+      _react2['default'].createElement(
+        'h1',
+        null,
+        this.state.count
+      ),
+      _react2['default'].createElement(
+        'h2',
+        null,
+        'State Object Debugger'
+      ),
+      _react2['default'].createElement(
+        'pre',
+        null,
+        JSON.stringify(this.state, null, 2)
+      )
+    );
+  }
+});
+
+// render the app
+_reactDom2['default'].render(_react2['default'].createElement(ComponentTest, { name: 'Loaded Using NPM' }), appTarget);
+
+// get random transcript. to be moved to it's own container
+var getRandom = function getRandom() {
+  $.ajax({
+    url: '/api/transcript/random/'
+  }).done(function (data) {
+    console.log(data);
+  });
 };
+getRandom();
 
-function nextPhraseButtons(transcriptPhrases) {
-    var target;
-    for (var x = 0; x < 3; x++) {
-        if (x === 0) {
-            target = document.getElementById('phrase1');
-            console.log(target);
-        } else if (x === 1) {
-            target = document.getElementById('phrase2');
-            console.log(target);
-        } else if (x === 2) {
-            target = document.getElementById('phrase3');
-            console.log(target);
-        }
-        target.textContent = transcriptData.phrases.shift().text;
-        if (target.classList.contains('list-group-item-danger')) {
-            target.classList.remove('list-group-item-danger');
-            target.classList.add('list-group-item-success');
-        }
-    }
-}
+// function reqListener () {
+//     console.log(this.responseText);
+// }
+
+// var swapClass;
+
+// document.getElementById("submit").onclick = nextPhraseButtons;
+
+// function swapClassEvent (event) {
+//     console.log('doing swap class');
+//     if (event.srcElement.classList.contains('list-group-item-success')) {
+//         event.srcElement.classList.remove('list-group-item-success');
+//         event.srcElement.classList.add('list-group-item-danger');
+//     }
+//     else if (event.srcElement.classList.contains('list-group-item-danger')) {
+//         event.srcElement.classList.remove('list-group-item-danger');
+//         event.srcElement.classList.add('list-group-item-success');
+//     }
+// }
+
+// var phraseAnchors = document.getElementsByClassName('list-group-item');
+
+// var i;
+// for (i = 0; i < phraseAnchors.length; i++) {
+//     phraseAnchors[i].onclick = swapClassEvent;
+// }
+
+// function mediaURL (transcript_id) {
+//     var mediaRequest = new XMLHttpRequest();
+//     var mediaData;
+//     mediaRequest.open('GET', '/api/media/' + transcript_id + '/');
+//     mediaRequest.send();
+//     mediaRequest.onreadystatechange = function () {
+//         var player = document.getElementById('player');
+//         var playersrc = document.getElementById('mp3src');
+//         if (mediaRequest.readyState == 4 && (mediaRequest.status == 200)) {
+//             mediaData = JSON.parse(mediaRequest.responseText);
+//             console.log(mediaData.media);
+//             playersrc.src = mediaData.media;
+//             player.load();
+//         }
+//     };
+// }
+
+// var transcriptRequest = new XMLHttpRequest();
+// var transcriptData;
+// transcriptRequest.open('GET', '/api/transcript/random/');
+// transcriptRequest.send();
+// transcriptRequest.onreadystatechange = function () {
+//     if (transcriptRequest.readyState == 4 && (transcriptRequest.status == 200)) {
+//         transcriptData = JSON.parse(transcriptRequest.responseText);
+//         mediaURL (transcriptData.transcript);
+//         nextPhraseButtons (transcriptData.phrases);
+//         document.getElementById('title').textContent = 'Title: ' + transcriptData.series;
+//         document.getElementById('station').textContent = 'Station: ' + transcriptData.station;
+//     }
+// };
+
+// function nextPhraseButtons (transcriptPhrases) {
+//     var target;
+//     for (var x=0; x < 3; x++) {
+//         if (x === 0) {
+//             target = document.getElementById('phrase1');
+//             console.log(target);
+//         }
+//         else if (x === 1) {
+//             target = document.getElementById('phrase2');
+//             console.log(target);
+//         }
+//         else if (x === 2) {
+//             target = document.getElementById('phrase3');
+//             console.log(target);
+//         }
+//         target.textContent = transcriptData.phrases.shift().text;
+//         if (target.classList.contains('list-group-item-danger')) {
+//             target.classList.remove('list-group-item-danger');
+//             target.classList.add('list-group-item-success');
+//         }
+//     }
+// }
 
 },{"react":172,"react-dom":29}],2:[function(require,module,exports){
 (function (process){
--- mla_game/front-end/javascript/main.js
+++ mla_game/front-end/javascript/main.js
@@ -1,95 +1,147 @@
+// imports
 import React from 'react'
 import ReactDOM from 'react-dom';
 window.React = React
 window.ReactDOM = ReactDOM
 
-// var mountNode = document.getElementById('target');
+var appTarget = document.getElementById('app');
+// component names should always begin with an uppercase letter
+// state = owned by current component
+// props = handed down from parent component
+/* use _ as a prefix for custom functions
+   // custom function
+   _clickHandler: function(){} 
+   // native to react    
+   getInitialState: function(){}
+*/
 
-// var HelloMessage = React.createClass({
-//   render: function() {
-//     return <div>React {this.props.name}</div>;
-//   }
-// });
-
-// ReactDOM.render(<HelloMessage name="Loaded Using NPM" />, mountNode);
-
-function reqListener () {
-    console.log(this.responseText);
-}
-
-var swapClass;
-
-document.getElementById("submit").onclick = nextPhraseButtons;
-
-function swapClassEvent (event) {
-    console.log('doing swap class');
-    if (event.srcElement.classList.contains('list-group-item-success')) {
-        event.srcElement.classList.remove('list-group-item-success');
-        event.srcElement.classList.add('list-group-item-danger');
-    }
-    else if (event.srcElement.classList.contains('list-group-item-danger')) {
-        event.srcElement.classList.remove('list-group-item-danger');
-        event.srcElement.classList.add('list-group-item-success');
-    }
-}
-
-var phraseAnchors = document.getElementsByClassName('list-group-item');
-
-var i;
-for (i = 0; i < phraseAnchors.length; i++) {
-    phraseAnchors[i].onclick = swapClassEvent;
-}
-
-function mediaURL (transcript_id) {
-    var mediaRequest = new XMLHttpRequest();
-    var mediaData;
-    mediaRequest.open('GET', '/api/media/' + transcript_id + '/');
-    mediaRequest.send();
-    mediaRequest.onreadystatechange = function () {
-        var player = document.getElementById('player');
-        var playersrc = document.getElementById('mp3src');
-        if (mediaRequest.readyState == 4 && (mediaRequest.status == 200)) {
-            mediaData = JSON.parse(mediaRequest.responseText);
-            console.log(mediaData.media);
-            playersrc.src = mediaData.media;
-            player.load();
-        }
+// test component
+var ComponentTest = React.createClass({
+  getInitialState: function(){
+    return {
+      key: 'value',
+      count:0,
     };
-}
+  },
 
-var transcriptRequest = new XMLHttpRequest();
-var transcriptData;
-transcriptRequest.open('GET', '/api/transcript/random/');
-transcriptRequest.send();
-transcriptRequest.onreadystatechange = function () {
-    if (transcriptRequest.readyState == 4 && (transcriptRequest.status == 200)) {
-        transcriptData = JSON.parse(transcriptRequest.responseText);
-        mediaURL (transcriptData.transcript);
-        nextPhraseButtons (transcriptData.phrases);
-        document.getElementById('title').textContent = 'Title: ' + transcriptData.series;
-        document.getElementById('station').textContent = 'Station: ' + transcriptData.station;
-    }
+  _clickHandler: function(){
+    this.setState({
+      count:this.state.count + 1,
+    })
+  },
+
+  render: function(){
+    return (
+      <div>
+        <h2>Simple React {this.props.name}</h2>
+        <button className="app-counter" onClick={this._clickHandler}>Change Count</button>
+        <h1>{this.state.count}</h1>
+        <h2>State Object Debugger</h2>
+        <pre>{JSON.stringify(this.state, null, 2)}</pre>
+      </div>
+    )
+  }
+})
+
+// render the app
+ReactDOM.render(
+  <ComponentTest name='Loaded Using NPM' />, 
+appTarget);
+
+
+
+// get random transcript. to be moved to it's own container
+var getRandom = function(){
+  $.ajax({
+    url: '/api/transcript/random/',
+  })
+  .done(function(data) {
+    console.log(data);
+  });
 };
+getRandom();
 
-function nextPhraseButtons (transcriptPhrases) {
-    var target;
-    for (var x=0; x < 3; x++) {
-        if (x === 0) {
-            target = document.getElementById('phrase1');
-            console.log(target);
-        }
-        else if (x === 1) {
-            target = document.getElementById('phrase2');
-            console.log(target);
-        }
-        else if (x === 2) {
-            target = document.getElementById('phrase3');
-            console.log(target);
-        }
-        target.textContent = transcriptData.phrases.shift().text;
-        if (target.classList.contains('list-group-item-danger')) {
-            target.classList.remove('list-group-item-danger');
-            target.classList.add('list-group-item-success');
-        }
-    }
-}
+
+
+
+
+// function reqListener () {
+//     console.log(this.responseText);
+// }
+
+// var swapClass;
+
+// document.getElementById("submit").onclick = nextPhraseButtons;
+
+// function swapClassEvent (event) {
+//     console.log('doing swap class');
+//     if (event.srcElement.classList.contains('list-group-item-success')) {
+//         event.srcElement.classList.remove('list-group-item-success');
+//         event.srcElement.classList.add('list-group-item-danger');
+//     }
+//     else if (event.srcElement.classList.contains('list-group-item-danger')) {
+//         event.srcElement.classList.remove('list-group-item-danger');
+//         event.srcElement.classList.add('list-group-item-success');
+//     }
+// }
+
+// var phraseAnchors = document.getElementsByClassName('list-group-item');
+
+// var i;
+// for (i = 0; i < phraseAnchors.length; i++) {
+//     phraseAnchors[i].onclick = swapClassEvent;
+// }
+
+// function mediaURL (transcript_id) {
+//     var mediaRequest = new XMLHttpRequest();
+//     var mediaData;
+//     mediaRequest.open('GET', '/api/media/' + transcript_id + '/');
+//     mediaRequest.send();
+//     mediaRequest.onreadystatechange = function () {
+//         var player = document.getElementById('player');
+//         var playersrc = document.getElementById('mp3src');
+//         if (mediaRequest.readyState == 4 && (mediaRequest.status == 200)) {
+//             mediaData = JSON.parse(mediaRequest.responseText);
+//             console.log(mediaData.media);
+//             playersrc.src = mediaData.media;
+//             player.load();
+//         }
+//     };
+// }
+
+// var transcriptRequest = new XMLHttpRequest();
+// var transcriptData;
+// transcriptRequest.open('GET', '/api/transcript/random/');
+// transcriptRequest.send();
+// transcriptRequest.onreadystatechange = function () {
+//     if (transcriptRequest.readyState == 4 && (transcriptRequest.status == 200)) {
+//         transcriptData = JSON.parse(transcriptRequest.responseText);
+//         mediaURL (transcriptData.transcript);
+//         nextPhraseButtons (transcriptData.phrases);
+//         document.getElementById('title').textContent = 'Title: ' + transcriptData.series;
+//         document.getElementById('station').textContent = 'Station: ' + transcriptData.station;
+//     }
+// };
+
+// function nextPhraseButtons (transcriptPhrases) {
+//     var target;
+//     for (var x=0; x < 3; x++) {
+//         if (x === 0) {
+//             target = document.getElementById('phrase1');
+//             console.log(target);
+//         }
+//         else if (x === 1) {
+//             target = document.getElementById('phrase2');
+//             console.log(target);
+//         }
+//         else if (x === 2) {
+//             target = document.getElementById('phrase3');
+//             console.log(target);
+//         }
+//         target.textContent = transcriptData.phrases.shift().text;
+//         if (target.classList.contains('list-group-item-danger')) {
+//             target.classList.remove('list-group-item-danger');
+//             target.classList.add('list-group-item-success');
+//         }
+//     }
+// }
