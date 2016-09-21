765e2e897c05e3b62b5f98da31c883ba033aab3e
Jay Thompson <jay_thompson@wgbh.org>
2016-09-21 15:53:13 -0400

    Account profiles with preferred station and subjects

--- mla_game/apps/accounts/admin.py
+++ mla_game/apps/accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    model = Profile
+
+
+admin.site.register(Profile, ProfileAdmin)
--- mla_game/apps/accounts/models.py
+++ mla_game/apps/accounts/models.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(
-        User
+    user = models.OneToOneField(User)
+    preferred_station = models.ForeignKey(
+        'transcript.Source',
+        default=None,
+        null=True,
+        blank=True
     )
+    preferred_topics = models.ManyToManyField('transcript.Topic')
