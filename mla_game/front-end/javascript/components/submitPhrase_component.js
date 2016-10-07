459b4cc41725bc36af1e44921728ca034941e96f
Paul Kinchla <paul_kinchla@wgbh.org>
2016-10-07 16:40:31 -0400

    submit phrase component

---
+++ mla_game/front-end/javascript/components/submitPhrase_component.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+var SubmitPhrase = React.createClass({
+
+  render: function(){
+    return (
+      <button type='submit' className='submit-phrase'>Submit Phrase</button>
+    )
+  }
+  
+});
+export default SubmitPhrase;
