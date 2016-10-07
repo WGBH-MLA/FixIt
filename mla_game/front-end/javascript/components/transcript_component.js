4e82c59484f5b35ea630c94cc4602bf03853f670
Paul Kinchla <paul_kinchla@wgbh.org>
2016-10-07 16:41:51 -0400

    included submit phrase component with conditional logic for active phrase

--- mla_game/front-end/javascript/components/transcript_component.js
+++ mla_game/front-end/javascript/components/transcript_component.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Audio from '../components/audio_component'
+import Submit from '../components/submitPhrase_component'
 
 var RandTranscriptUI = React.createClass({
 
@@ -33,11 +34,12 @@ var RandTranscriptUI = React.createClass({
       <pre>{JSON.stringify(this.state, null, 2)}</pre>
       <Audio src={this.props.media_url} />
        <ul className='phrase-list'>
-        {this.props.phrases.map(function (phrase){
+        {this.props.phrases.map(function(phrase){
           return(
           <li key={phrase.pk}>
             <button className='play-button' id={phrase.start_time} onClick={this._playPhrase.bind(this, phrase.start_time)}>Play</button>
             <button className={this.state.currentPhrase === phrase.pk ? 'incorrect phrase' : 'un-marked phrase'} onClick={this._selectPhrase.bind(this, phrase.pk)} id={phrase.pk}>{phrase.text}</button>
+            {this.state.currentPhrase === phrase.pk ? <Submit /> : null }
           </li>)
         }.bind(this))}
        </ul>
