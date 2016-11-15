326e3d220b60d836bb1097856ba1dce857b61abb
Jay Thompson <jay_thompson@wgbh.org>
2016-11-15 14:58:37 -0500

    tiny bit cleaner

--- mla_game/apps/accounts/admin.py
+++ mla_game/apps/accounts/admin.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 from .models import Profile
 
 
+@admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     model = Profile
-
-
-admin.site.register(Profile, ProfileAdmin)
+    fields = ('user', 'preferred_stations', 'preferred_topics')
