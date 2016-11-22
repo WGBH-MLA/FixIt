330dfe981e77766e2cfa340cb381e9faadb6a6e8
Paul Kinchla <paul_kinchla@wgbh.org>
2016-11-22 09:59:09 -0500

    addition of play button to audio component

--- mla_game/front-end/javascript/components/audio_component.js
+++ mla_game/front-end/javascript/components/audio_component.js
@@ -4,7 +4,15 @@ var AudioUI = React.createClass({
 
   render: function(){
     return (
-      <audio className='audio-player' src={this.props.src} controls></audio>
+      <div>
+        <button className='play-button'>
+          <svg class="video-overlay-play-button" viewBox="0 0 200 200" alt="Play video">
+            <circle cx="100" cy="100" r="90" fill="none" stroke-width="15" stroke="#fff"/>
+            <polygon points="70, 55 70, 145 145, 100" fill="#fff"/>
+          </svg>
+        </button>  
+        <audio className='audio-player' src={this.props.src}></audio>
+      </div>
     )
   }
   
