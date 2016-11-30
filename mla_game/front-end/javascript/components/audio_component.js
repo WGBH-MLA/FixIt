85854883ac7ca20fe5278f241acb945bbdcd9c31
Paul Kinchla <paul_kinchla@wgbh.org>
2016-11-30 16:39:43 -0500

    addition of toggle event for play button

--- mla_game/front-end/javascript/components/audio_component.js
+++ mla_game/front-end/javascript/components/audio_component.js
@@ -5,6 +5,7 @@ class AudioUI extends  React.Component{
   constructor(){
     super();
     this._playAudio = this._playAudio.bind(this); 
+    this._togglePlay = this._togglePlay.bind(this); 
   }
 
   _playAudio(){
@@ -15,14 +16,34 @@ class AudioUI extends  React.Component{
     }
   }
 
+  _togglePlay() {
+    if(this.props.isPlaying) {
+      return(
+        <g>
+          <rect fill="#fff" width="25.577312" height="88.712906" x="65" y="55.643555" />
+          <rect fill="#fff" width="25.577312" height="88.712906" x="110" y="55.643555" />
+        </g>
+      )
+    } else {
+      return(
+        <polygon points="70, 55 70, 145 145, 100" fill="#fff"/>
+      )
+    } 
+  }
+
+  componentDidMount() {
+    this._togglePlay();
+  }
+
   render(){
+    const { isPlaying } = this.props;
     return (
       <div className='audio'>
-        <h1>{this.props.isPlaying }</h1>
         <button className='play-button' onClick={this._playAudio}>
+          {this._togglePlay()}
           <svg className="play-icon" viewBox="0 0 200 200">
             <circle cx="100" cy="100" r="90" fill="none" strokeWidth="15" stroke="#fff"/>
-            <polygon points="70, 55 70, 145 145, 100" fill="#fff"/>
+            {this._togglePlay()}
           </svg>
         </button>  
         <audio ref={(audio) => {this.audioPlayer = audio}} className="audio-player" src={this.props.src}></audio>
