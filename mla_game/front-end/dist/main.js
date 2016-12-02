fe9c33759ded500cc586a31c51bd8348db1606b0
Paul Kinchla <paul_kinchla@wgbh.org>
2016-12-02 17:14:36 -0500

    addition of loading screen

--- mla_game/front-end/dist/main.js
+++ mla_game/front-end/dist/main.js
@@ -1116,11 +1116,14 @@ var TranscriptUI = (function (_React$Component) {
     this._selectPhrase = this._selectPhrase.bind(this);
     this._syncAudio = this._syncAudio.bind(this);
     this._playPhrase = this._playPhrase.bind(this);
+    this._delayRender = this._delayRender.bind(this);
+    this._renderGame = this._renderGame.bind(this);
 
     this.state = {
       currentPhrase: [],
       currentTime: 0,
-      isPlaying: false
+      isPlaying: false,
+      loaded: false
     };
   }
 
@@ -1147,50 +1150,99 @@ var TranscriptUI = (function (_React$Component) {
       media.play();
     }
   }, {
-    key: 'render',
-    value: function render() {
+    key: '_delayRender',
+    value: function _delayRender() {
+      this.setState({
+        loaded: true
+      });
+    }
+  }, {
+    key: '_renderGame',
+    value: function _renderGame() {
       var _this = this;
 
+      if (this.state.loaded) {
+        return _react2['default'].createElement(
+          'div',
+          null,
+          _react2['default'].createElement(
+            'div',
+            { className: 'app-content' },
+            _react2['default'].createElement(
+              'h3',
+              null,
+              'State Object Debugger'
+            ),
+            _react2['default'].createElement(
+              'pre',
+              null,
+              JSON.stringify(this.state, null, 2)
+            ),
+            _react2['default'].createElement(
+              'div',
+              { className: 'game-meta' },
+              _react2['default'].createElement(_componentsAudio_component2['default'], { src: this.props.media_url, _syncAudio: this._syncAudio, isPlaying: this.state.isPlaying }),
+              _react2['default'].createElement(_componentsGame_meta2['default'], { meta: this.props.meta, aapb_link: this.props.aapb_link })
+            ),
+            _react2['default'].createElement(
+              'ul',
+              { className: 'game-phrase-list' },
+              Object.keys(this.props.phrases).map(function (key) {
+                return _react2['default'].createElement(_componentsPhrase_list2['default'], { key: key, _playPhrase: _this._playPhrase, _selectPhrase: _this._selectPhrase, time: _this.state.currentTime, index: key, details: _this.props.phrases[key] });
+              }),
+              this.props.phrases.length / 8
+            )
+          ),
+          _react2['default'].createElement(
+            'div',
+            { className: 'game-footer' },
+            _react2['default'].createElement(
+              'div',
+              null,
+              _react2['default'].createElement('progress', { max: '100', value: '20' })
+            )
+          )
+        );
+      } else {
+        return _react2['default'].createElement(
+          'div',
+          { className: 'app-content loading-screen' },
+          _react2['default'].createElement(
+            'svg',
+            { className: 'loading-animation', viewBox: '0 0 100 100', preserveAspectRatio: 'xMidYMid' },
+            _react2['default'].createElement('rect', { x: '0', y: '0', width: '100', height: '100', fill: 'none' }),
+            _react2['default'].createElement(
+              'defs',
+              null,
+              _react2['default'].createElement(
+                'filter',
+                { id: 'uil-ring-shadow', x: '-100%', y: '-100%', width: '300%', height: '300%' },
+                _react2['default'].createElement('feOffset', { result: 'offOut', 'in': 'SourceGraphic', dx: '0', dy: '0' }),
+                _react2['default'].createElement('feGaussianBlur', { result: 'blurOut', 'in': 'offOut', stdDeviation: '0' }),
+                _react2['default'].createElement('feBlend', { 'in': 'SourceGraphic', in2: 'blurOut', mode: 'normal' })
+              )
+            ),
+            _react2['default'].createElement(
+              'path',
+              { d: 'M10,50c0,0,0,0.5,0.1,1.4c0,0.5,0.1,1,0.2,1.7c0,0.3,0.1,0.7,0.1,1.1c0.1,0.4,0.1,0.8,0.2,1.2c0.2,0.8,0.3,1.8,0.5,2.8 c0.3,1,0.6,2.1,0.9,3.2c0.3,1.1,0.9,2.3,1.4,3.5c0.5,1.2,1.2,2.4,1.8,3.7c0.3,0.6,0.8,1.2,1.2,1.9c0.4,0.6,0.8,1.3,1.3,1.9 c1,1.2,1.9,2.6,3.1,3.7c2.2,2.5,5,4.7,7.9,6.7c3,2,6.5,3.4,10.1,4.6c3.6,1.1,7.5,1.5,11.2,1.6c4-0.1,7.7-0.6,11.3-1.6 c3.6-1.2,7-2.6,10-4.6c3-2,5.8-4.2,7.9-6.7c1.2-1.2,2.1-2.5,3.1-3.7c0.5-0.6,0.9-1.3,1.3-1.9c0.4-0.6,0.8-1.3,1.2-1.9 c0.6-1.3,1.3-2.5,1.8-3.7c0.5-1.2,1-2.4,1.4-3.5c0.3-1.1,0.6-2.2,0.9-3.2c0.2-1,0.4-1.9,0.5-2.8c0.1-0.4,0.1-0.8,0.2-1.2 c0-0.4,0.1-0.7,0.1-1.1c0.1-0.7,0.1-1.2,0.2-1.7C90,50.5,90,50,90,50s0,0.5,0,1.4c0,0.5,0,1,0,1.7c0,0.3,0,0.7,0,1.1 c0,0.4-0.1,0.8-0.1,1.2c-0.1,0.9-0.2,1.8-0.4,2.8c-0.2,1-0.5,2.1-0.7,3.3c-0.3,1.2-0.8,2.4-1.2,3.7c-0.2,0.7-0.5,1.3-0.8,1.9 c-0.3,0.7-0.6,1.3-0.9,2c-0.3,0.7-0.7,1.3-1.1,2c-0.4,0.7-0.7,1.4-1.2,2c-1,1.3-1.9,2.7-3.1,4c-2.2,2.7-5,5-8.1,7.1 c-0.8,0.5-1.6,1-2.4,1.5c-0.8,0.5-1.7,0.9-2.6,1.3L66,87.7l-1.4,0.5c-0.9,0.3-1.8,0.7-2.8,1c-3.8,1.1-7.9,1.7-11.8,1.8L47,90.8 c-1,0-2-0.2-3-0.3l-1.5-0.2l-0.7-0.1L41.1,90c-1-0.3-1.9-0.5-2.9-0.7c-0.9-0.3-1.9-0.7-2.8-1L34,87.7l-1.3-0.6 c-0.9-0.4-1.8-0.8-2.6-1.3c-0.8-0.5-1.6-1-2.4-1.5c-3.1-2.1-5.9-4.5-8.1-7.1c-1.2-1.2-2.1-2.7-3.1-4c-0.5-0.6-0.8-1.4-1.2-2 c-0.4-0.7-0.8-1.3-1.1-2c-0.3-0.7-0.6-1.3-0.9-2c-0.3-0.7-0.6-1.3-0.8-1.9c-0.4-1.3-0.9-2.5-1.2-3.7c-0.3-1.2-0.5-2.3-0.7-3.3 c-0.2-1-0.3-2-0.4-2.8c-0.1-0.4-0.1-0.8-0.1-1.2c0-0.4,0-0.7,0-1.1c0-0.7,0-1.2,0-1.7C10,50.5,10,50,10,50z', fill: '#ffae18', filter: 'url(#uil-ring-shadow)' },
+              _react2['default'].createElement('animateTransform', { attributeName: 'transform', type: 'rotate', from: '0 50 50', to: '360 50 50', repeatCount: 'indefinite', dur: '1.5s' })
+            )
+          )
+        );
+      }
+    }
+  }, {
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      setTimeout(this._delayRender, 5000);
+    }
+  }, {
+    key: 'render',
+    value: function render() {
       return _react2['default'].createElement(
         'div',
         null,
-        _react2['default'].createElement(
-          'div',
-          { className: 'app-content' },
-          _react2['default'].createElement(
-            'h3',
-            null,
-            'State Object Debugger'
-          ),
-          _react2['default'].createElement(
-            'pre',
-            null,
-            JSON.stringify(this.state, null, 2)
-          ),
-          _react2['default'].createElement(
-            'div',
-            { className: 'game-meta' },
-            _react2['default'].createElement(_componentsAudio_component2['default'], { src: this.props.media_url, _syncAudio: this._syncAudio, isPlaying: this.state.isPlaying }),
-            _react2['default'].createElement(_componentsGame_meta2['default'], { meta: this.props.meta, aapb_link: this.props.aapb_link })
-          ),
-          _react2['default'].createElement(
-            'ul',
-            { className: 'game-phrase-list' },
-            Object.keys(this.props.phrases).map(function (key) {
-              return _react2['default'].createElement(_componentsPhrase_list2['default'], { key: key, _playPhrase: _this._playPhrase, _selectPhrase: _this._selectPhrase, time: _this.state.currentTime, index: key, details: _this.props.phrases[key] });
-            }),
-            this.props.phrases.length / 8
-          )
-        ),
-        _react2['default'].createElement(
-          'div',
-          { className: 'game-footer' },
-          _react2['default'].createElement(
-            'div',
-            null,
-            _react2['default'].createElement('progress', { max: '100', value: '20' })
-          )
-        )
+        this._renderGame()
       );
     }
   }]);
