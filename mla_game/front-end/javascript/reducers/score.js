51b327e8fb5273e48fc1a6eef900bff73264eeaa
Paul Kinchla <paul_kinchla@wgbh.org>
2017-01-05 17:14:20 -0500

    setup for working score reducer

--- mla_game/front-end/javascript/reducers/score.js
+++ mla_game/front-end/javascript/reducers/score.js
@@ -4,14 +4,16 @@
 // 2. copy of current state
 
 function score(state = [], action) {
-  console.log(state, action);
-
-  
   switch(action.type){
     case 'UPDATE_SCORE' :
+      return {
+        // take a copy of state
+        ...state.totalScore, 
+        // increment score based on amount callback
+        totalScore:state.totalScore + action.amount
+      }
     default:
       return state;
   }
 }
-
 export default score;
