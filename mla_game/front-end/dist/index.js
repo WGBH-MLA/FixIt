9b38f4d6d8fd7377a350c68d3a1bfaab5ae7ace0
Paul Kinchla <paul_kinchla@wgbh.org>
2017-01-17 16:10:58 -0500

    waiting for ui added back to advance round

--- mla_game/front-end/dist/index.js
+++ mla_game/front-end/dist/index.js
@@ -1381,7 +1381,7 @@ var GameOne = (function (_React$Component) {
       // copy state
       var wrongPhrases = _extends({}, this.state.wrongPhrases);
       // disable advance round for three seconds when round updates
-      // this.props.wait(3000);
+      this.props.wait(3000);
 
       // check if the round has ended. if so change state.
       // if not push other things to state like the score and play the media   
