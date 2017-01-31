52a7c6ecd103ea155a70cf4c342caed093ad21b9
Paul Kinchla <paul_kinchla@wgbh.org>
2017-01-31 16:49:56 -0500

    removal of unused call to helpers js

--- mla_game/front-end/javascript/store.js
+++ mla_game/front-end/javascript/store.js
@@ -1,13 +1,11 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import createLogger from 'redux-logger'
-import { getUserEndpoint } from './helpers'
 
 //import the root reducer
  import rootReducer from './reducers/index'
 
 const loggerMiddleware = createLogger()
-
 
 export default function configureStore(defaultState) {
   return createStore(
