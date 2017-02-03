1b2d1e9e227398f2df6820e571a694514a6d707d
Jay Thompson <jay_thompson@wgbh.org>
2017-02-03 15:34:49 -0500

    Move signals into signals.py

--- mla_game/apps/accounts/signals.py
+++ mla_game/apps/accounts/signals.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.signals import user_logged_in
 from django.dispatch import receiver
+from django.db.models.signals import m2m_changed
 
 from .models import Profile
+from .tasks import update_transcript_picks, update_partial_or_complete_transcripts
 
 
 @receiver(user_logged_in)
@@ -14,3 +16,17 @@ def get_or_create_profile(sender, user, request, **kwargs):
         profile.save()
 
     return profile
+
+
+def create_update_transcript_picks_task(sender, instance, action, reverse, model, pk_set, using, **kwargs):
+    if action == 'post_add' or action == 'post_remove':
+        update_transcript_picks(instance.user)
+
+
+def considered_phrases_changed(sender, instance, action, reverse, model, pk_set, using, **kwargs):
+    if action == 'post_add':
+        update_partial_or_complete_transcripts(instance.user, pk_set)
+
+
+m2m_changed.connect(create_update_transcript_picks_task, sender=Profile.preferred_stations.through)
+m2m_changed.connect(considered_phrases_changed, sender=Profile.considered_phrases.through)
--- mla_game/apps/accounts/tasks.py
+++ mla_game/apps/accounts/tasks.py
@@ -1,7 +1,6 @@
 import logging
 import datetime
 
-from django.db.models.signals import m2m_changed
 from django.contrib.auth.models import User
 
 from huey.contrib.djhuey import crontab, db_periodic_task, db_task
@@ -241,17 +240,3 @@ def update_leaderboard():
     ]
 
     Leaderboard.objects.create(leaderboard=leaderboard)
-
-
-def create_update_transcript_picks_task(sender, instance, action, reverse, model, pk_set, using, **kwargs):
-    if action == 'post_add' or action == 'post_remove':
-        update_transcript_picks(instance.user)
-
-
-def considered_phrases_changed(sender, instance, action, reverse, model, pk_set, using, **kwargs):
-    if action == 'post_add':
-        update_partial_or_complete_transcripts(instance.user, pk_set)
-
-
-m2m_changed.connect(create_update_transcript_picks_task, sender=Profile.preferred_stations.through)
-m2m_changed.connect(considered_phrases_changed, sender=Profile.considered_phrases.through)
