8d8d7e3145aa9201f9a0100223281af43b79fb4b
Jay Thompson <jay_thompson@wgbh.org>
2017-03-03 13:46:33 -0500

    Update picks when station prefs change

--- mla_game/apps/accounts/signals.py
+++ mla_game/apps/accounts/signals.py
@@ -1,9 +1,14 @@
+import logging
+
 from django.contrib.auth.signals import user_logged_in
 from django.dispatch import receiver
 from django.db.models.signals import m2m_changed
 
 from .models import Profile
 from .tasks import update_transcript_picks, update_partial_or_complete_transcripts
+
+
+django_log = logging.getLogger('django')
 
 
 @receiver(user_logged_in)
@@ -14,7 +19,6 @@ def get_or_create_profile(sender, user, request, **kwargs):
     if created:
         profile.username = user.username
         profile.save()
-
     return profile
 
 
@@ -29,4 +33,5 @@ def considered_phrases_changed(sender, instance, action, reverse, model, pk_set,
 
 
 m2m_changed.connect(create_update_transcript_picks_task, sender=Profile.preferred_stations.through)
+m2m_changed.connect(create_update_transcript_picks_task, sender=Profile.preferred_topics.through)
 m2m_changed.connect(considered_phrases_changed, sender=Profile.considered_phrases.through)
