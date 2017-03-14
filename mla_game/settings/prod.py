06f03cfab6fa38e4c7eab26f7d0bd25506db8238
Jay Thompson <jay_thompson@wgbh.org>
2017-03-14 12:07:43 -0400

    Initial settings for prod server

---
+++ mla_game/settings/prod.py
@@ -0,0 +1,83 @@
+from .base import *
+import os
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['fixit.americanarchive.org', 'fixit.wgbh-mla.org']
+
+LOG_DIRECTORY = '/home/wgbh/logs'
+
+STATIC_ROOT = '/var/nginx/webroot/static'
+
+PUA_CACHE_DIRECTORY = '/home/wgbh/pua_cache/'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'fixit-test2.cmrjt7rckp5r.us-east-1.rds.amazonaws.com',
+        'NAME': 'fixit_test',
+        'USER': 'wgbh',
+        'PASSWORD': os.environ['PG_PASS'],
+    },
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': '{}/django.log'.format(LOG_DIRECTORY),
+        },
+        'pua_scraper': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': '{}/pua_scraper.log'.format(LOG_DIRECTORY),
+        },
+        'pua_errors': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': '{}/pua_errors.log'.format(LOG_DIRECTORY),
+        },
+        'pua_stats': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': '{}/pua_stats.csv'.format(LOG_DIRECTORY),
+        },
+        'metadata_errors': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': '{}/metadata_error.log'.format(LOG_DIRECTORY),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'pua_scraper': {
+            'handlers': ['pua_scraper'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'pua_errors': {
+            'handlers': ['pua_errors'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'pua_stats': {
+            'handlers': ['pua_stats'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'metadata_errors': {
+            'handlers': ['metadata_errors'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
