f6448e8a3b64673c744ae5b8b20e98db1e01d9ac
Jay Thompson <jay_thompson@wgbh.org>
2017-03-17 11:48:03 -0400

    Add missing string placeholder

--- mla_game/settings/prod.py
+++ mla_game/settings/prod.py
@@ -23,7 +23,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
-AWS_S3_CUSTOM_DOMAIN = '.s3-website-us-east-1.amazonaws.com'.format(
+AWS_S3_CUSTOM_DOMAIN = '{}.s3-website-us-east-1.amazonaws.com'.format(
     AWS_STORAGE_BUCKET_NAME
 )
 
