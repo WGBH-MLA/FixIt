aad38c38e59655c98d0833434ba6faacc124ea6f
Jay Thompson <jay_thompson@wgbh.org>
2017-03-17 11:30:58 -0400

    Add settings and pkgs for static files in S3

--- mla_game/settings/prod.py
+++ mla_game/settings/prod.py
@@ -1,6 +1,8 @@
 from .base import *
 import os
 
+INSTALLED_APPS += ('storages',)
+
 SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = False
@@ -9,11 +11,24 @@ ALLOWED_HOSTS = ['fixit.americanarchive.org', 'fixit.wgbh-mla.org']
 
 LOG_DIRECTORY = '/home/wgbh/logs'
 
-STATIC_ROOT = '/var/nginx/webroot/static'
-
 PUA_CACHE_DIRECTORY = '/home/wgbh/pua_cache/'
 
 GA_CODE = os.environ['GA_CODE']
+
+AWS_HEADERS = {
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'Cache-Control': 'max-age=94608000',
+}
+AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+
+AWS_S3_CUSTOM_DOMAIN = '.s3-website-us-east-1.amazonaws.com'.format(
+    AWS_STORAGE_BUCKET_NAME
+)
+
+STATIC_URL = 'http://{}/'.format(AWS_S3_CUSTOM_DOMAIN)
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 DATABASES = {
     'default': {
