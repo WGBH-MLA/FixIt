6b64645992aa3d278b0efdef76eb4da65b59e291
Jay Thompson <jay_thompson@wgbh.org>
2017-03-17 13:12:21 -0400

    Fix static settings

--- mla_game/settings/prod.py
+++ mla_game/settings/prod.py
@@ -23,11 +23,11 @@ AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
-AWS_S3_CUSTOM_DOMAIN = '{}.s3-website-us-east-1.amazonaws.com'.format(
+AWS_S3_CUSTOM_DOMAIN = 's3.amazonaws.com/{}'.format(
     AWS_STORAGE_BUCKET_NAME
 )
 
-STATIC_URL = 'http://{}/'.format(AWS_S3_CUSTOM_DOMAIN)
+STATIC_URL = 'https://s3.amazonaws.com/{}/'.format(AWS_S3_CUSTOM_DOMAIN)
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 DATABASES = {
