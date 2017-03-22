d4731dd3c72a64fb6581d5e0625a66106eb1deb4
Jay Thompson <jay_thompson@wgbh.org>
2017-03-22 15:51:23 -0400

    Newrelic in manage.py for huey monitoring

--- manage.py
+++ manage.py
@@ -6,5 +6,12 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mla_game.settings")
 
     from django.core.management import execute_from_command_line
+    from django.conf import settings
+
+    if hasattr(settings, 'NEWRELIC_CONFIG_PATH'):
+        import newrelic.agent
+        newrelic.agent.initialize(
+            settings.NEWRELIC_CONFIG_PATH, settings.NEWRELIC_ENV
+        )
 
     execute_from_command_line(sys.argv)
