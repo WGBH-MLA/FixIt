f47d7d80e88312e0679ddb7198d2c978434e1e16
Paul Kinchla <paul_kinchla@wgbh.org>
2017-04-12 16:26:50 -0400

    changed media links in game to open in a new tab

--- mla_game/front-end/javascript/components/partials/game_meta.js
+++ mla_game/front-end/javascript/components/partials/game_meta.js
@@ -27,7 +27,7 @@ class GameMeta extends React.Component {
         {this._metaElement(meta.station_name)}
         {this._metaElement(meta.broadcast_date)}
         <dd className="delta">
-          <a href={aapb_link}>
+          <a href={aapb_link} target="_blank">
             <svg className="aapb-link-icon" viewBox="0 0 200 200">
               <title>AAPB Link</title>
               <rect x="0" y="40.8" fill="#6D6E70" width="130.6" height="119"/>
