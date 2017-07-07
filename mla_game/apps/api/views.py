772fd34b33f0299e553fd7d67c52d2472a538fa7
Jay Thompson <jay_thompson@wgbh.org>
2017-07-07 15:51:50 -0400

    Remove unused transcript route

--- mla_game/apps/api/views.py
+++ mla_game/apps/api/views.py
@@ -55,14 +55,6 @@ class TranscriptViewSet(viewsets.ModelViewSet):
     serializer_class = TranscriptSerializer
 
     @list_route()
-    def user_transcripts(self, request):
-        transcripts = Transcript.objects.for_user(
-            request.user
-        )
-        serializer = self.get_serializer(transcripts, many=True)
-        return Response(serializer.data)
-
-    @list_route()
     def random(self, request):
         transcript = Transcript.objects.random_transcript()
         serializer = self.get_serializer(transcript, many=True)
