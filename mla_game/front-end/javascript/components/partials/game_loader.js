838b89ca671a66a00f7d962e1099340aa742b02f
Paul Kinchla <paul_kinchla@wgbh.org>
2018-01-31 13:10:32 -0500

    game message updates and username display fix for game menu

--- mla_game/front-end/javascript/components/partials/game_loader.js
+++ mla_game/front-end/javascript/components/partials/game_loader.js
@@ -27,19 +27,21 @@ render(){
         gameClass = classNames({
           'loading-area-action': true,
           'gameone': gameNumber === 1
-        })        
+        })
+
+    // set message and icon for everygame
+    message = <MarkdownRenderer markdown={this.props.message} />
+    icon = <svg className='notify-icon' viewBox="0 0 416 416"><title>Notification</title><path d="M208,416c23.573,0,42.667-19.093,42.667-42.667h-85.333C165.333,396.907,184.427,416,208,416z"/><path d="M336,288V181.333c0-65.6-34.88-120.32-96-134.827V32c0-17.707-14.293-32-32-32s-32,14.293-32,32v14.507 c-61.12,14.507-96,69.227-96,134.827V288l-42.667,42.667V352h341.333v-21.333L336,288z"/></svg>    
 
     // return game specific data
     if(gameNumber === 1) {
       gameSpecificData = <div><dt>Phrases with errors</dt><dd>{gameData.phrases_with_errors}</dd></div>
-      message = <MarkdownRenderer markdown={this.props.message} />
-      icon = <svg className='notify-icon' viewBox="0 0 416 416"><title>Notification</title><path d="M208,416c23.573,0,42.667-19.093,42.667-42.667h-85.333C165.333,396.907,184.427,416,208,416z"/><path d="M336,288V181.333c0-65.6-34.88-120.32-96-134.827V32c0-17.707-14.293-32-32-32s-32,14.293-32,32v14.507 c-61.12,14.507-96,69.227-96,134.827V288l-42.667,42.667V352h341.333v-21.333L336,288z"/></svg>
     } 
     else if( gameNumber === 2) {
       gameSpecificData = <div><dt>Suggested corrections</dt><dd>{gameData.suggested_corrections}</dd></div>
     }  
     else {
-      gameSpecificData = <div><dt>Validated corrections</dt><dd>{gameData.validated_corrections}</dd></div> 
+      gameSpecificData = <div><dt>Validated corrections</dt><dd>{gameData.validated_corrections}</dd></div>
     }
 
     if(noData) {
